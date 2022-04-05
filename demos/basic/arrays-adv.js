cded486cb2f6d047c009dfd5f0ef7660cb27d2df
pablof@tecnoshare.com <pablof@tecnoshare.com>
2022-04-04 22:23:16 -0300

    clase 4

--- demos/basic/arrays-adv.js
+++ demos/basic/arrays-adv.js
@@ -5,28 +5,74 @@ const inventors = [
     { first: 'Marie', last: 'Curie', year: 1867 },
     { first: 'Johannes', last: 'Kepler', year: 1571 },
     { first: 'Nicolaus', last: 'Copernicus', year: 1473 },
-    { first: 'Max', last: 'Planck', year: 1858 },
+    { first: 'Max', last: 'Planck', year: 1858 }
   ];
 
-  // TODO 1: Función para mostrar por consola en formato tabular
+  // TODO 1: Desarrollar Función para mostrar por consola en formato tabular
+  function printInventors(inventors){
+     console.log(`Nombre      Apellido    Año`);
+     console.log(`----------------------------`);
+     inventors.forEach(inventor =>
+         console.log(`${inventor.first.padEnd(12, " ")}${inventor.last.padEnd(12," ")}${inventor.year}`)
+     );
+     //inventors.forEach(miCallback);
+  }
+
+  // La forma larga
+//   function miCallback(inventor){
+//      console.log(`${inventor.first}     ${inventor.last}    ${inventor.year}`);
+//   }
 
   // TODO 2: Mostrar por consola los inventores nacidos antes del 1800
+  //printInventors(inventors.filter(inventor => inventor.year < 1800));
+
+//   printInventors(inventors.filter( function(inventor) {
+//       return inventor.year < 1800;
+//   }));
 
   // TODO 3: Mostrar por consola los inventores cuyo apellido empiece con C
+//   printInventors(
+//       inventors.filter(inventor => inventor.last.startsWith("C"))
+//   );
 
-  // TODO 4: Mostrar por consola Convertir el apellido en mayusculas
+  // TODO 4: Mostrar por consola convirtiendo el apellido en mayusculas
+//   printInventors(
+//       inventors.map(inventor => 
+//          ({first: inventor.first, last: inventor.last.toUpperCase(), year: inventor.year})
+//       )
+//   );
 
   // TODO 5: Mostrar aplicando 3 y 4
+//   printInventors(
+//      inventors
+//          .filter(inventor => inventor.last.startsWith("C"))
+//          .map(inventor => ({first: inventor.first, last: inventor.last.toUpperCase(), year: inventor.year}))
+//   )
 
   // TODO 6: Buscar el inventor Kepler y retornar el objeto
+//   console.log(
+//       inventors.find(inventor => inventor.last == "Kepler")
+//   );
 
-  // TODO 7: Hay algun inventor nacido en 1858
+  // TODO 7: Hay algun inventor nacido en 1858?
+//   console.log(
+//    inventors.some(inventor => inventor.year === 1858)
+//   );
 
-  // TODO 8: Todos los inventores nacieron despues de 1500
+  // TODO 8: Todos los inventores nacieron despues de 1500?
+  console.log(
+      inventors.every(inventor => inventor.year >= 1500)
+  );
 
   // TODO 9: Mostrar por consola los inventores ordenados ascendetemente por fecha de nacimiento
+  printInventors(
+      inventors.sort((inventorA, inventorB) => inventorA.year < inventorB.year? 1: -1)
+  );
 
   // TODO 10: Ordenar los inventores por apellido
+  printInventors(
+   inventors.sort((inventorA, inventorB) => inventorA.last > inventorB.last ? 1: -1)
+);
 
   // TODO 11: Converir el array al formato:
   /**
@@ -40,5 +86,13 @@ const inventors = [
      * ]
      * 
   */ 
+ console.log(
+    inventors.map( inventor =>
+       ({
+          name: {first: inventor.first, last: inventor.last},
+          year: inventor.year
+       })
+    )
+ );
 
   
---
+++ demos/basic/destructuring.js
@@ -0,0 +1,37 @@
+const movie = {
+    _id: "5eab6525943e6ac68387223d",
+    title: "Pirates of the Caribbean: Dead Man's Chest",
+    year: 2006,
+    rated: "PG-13",
+    runtime: 151.0,
+    countries:["USA"],
+    genres:["Action","Adventure","Fantasy"],
+    director:"Gore Verbinski",
+    writers:["Ted Elliott","Terry Rossio","Ted Elliott","Terry Rossio","Stuart Beattie","Jay Wolpert"],
+    actors:["Johnny Depp","Orlando Bloom","Keira Knightley","Jack Davenport"],
+    plot:"Jack Sparrow races to recover the heart of Davy Jones to avoid enslaving his soul to Jones' service, as other friends and foes seek the heart for their own agenda as well.",
+    poster:"http://ia.media-imdb.com/images/M/MV5BMTcwODc1MTMxM15BMl5BanBnXkFtZTYwMDg1NzY3._V1_SX300.jpg",
+    imdb: {
+            id:"tt0383574",
+            rating: 7.3,
+            votes: 492854.0
+        },
+    tomato: {
+        meter: 54.0,
+        image:"rotten",
+        rating:6.0,
+        reviews: 219.0,
+        fresh: 118.0,
+        consensus: "Gone is Depp's unpredictability and much of the humor and originality of the first movie.",
+        userMeter: 72.0,
+        userRating: 3.9,
+        userReviews: 1845112.0 
+    },
+    metacritic: 53.0,
+    awards: {
+        wins:41.0,
+        nominations:48.0,
+        text:"Won 1 Oscar. Another 41 wins & 48 nominations."
+    },
+    type: "movie"
+}
